ae6da42b462146073fcf5d56d8458a38316ee6eb
SanceiLaks <39582793+Sanceilaks@users.noreply.github.com>
2020-11-16 00:44:17 +0300

    Add logger

---
+++ deps/include/logger.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+
+#ifndef LAPI
+#define LAPI
+#endif
+
+
+namespace logger
+{
+	enum log_lvl
+	{
+		msg = 0,
+		warning,
+		error
+	};
+	/*
+	 * For custom
+	 * log_lvl_text[log_lvl::msg] = "Custom";
+	 */
+	static const char* log_lvl_text[] = 
+	{
+		"[L]",
+		"[W]",
+		"[E]"
+	};
+
+	typedef void(*log_func)(const std::string& text, log_lvl _log_lvl);
+	
+	LAPI void set_log_file(const std::string& file);
+	LAPI void log(const std::string& text, log_lvl _log_lvl = msg);
+	LAPI void set_log_in_file(bool state);
+	/*
+	 * Set your own log func. For set std - set_log_func(nullptr);
+	 */
+	LAPI void set_log_func(log_func func);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,27 @@
 
 #include "hack_core.h"
 
+#include <fstream>
+
+#include <logger.h>
+
 
 void init()
 {
-	freopen("C:\\LemiCSGO\\log", "w", stdout);
+	//freopen("", "w", stdout);
 	
 	if (!hack_core::init())
 		fprintf(stderr, "hack_core::init error!\n");
 
-	fprintf(stdout, "Hello from csgo stdout!\n");
-	std::cout << "Hello from csgo stdout!\n";
+	logger::log("Hello from csgo!");
 }
 
 
-BOOL WINAPI dll_main(HINSTANCE dll, const DWORD reason, LPVOID reserved)
-{
+BOOL APIENTRY DllMain(HMODULE dll, DWORD reason, LPVOID reserved)
+{	
+	DisableThreadLibraryCalls(dll);
 	if (reason == DLL_PROCESS_ATTACH)
 	{
-		DisableThreadLibraryCalls(dll);
 		CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(init), nullptr, 0, 0);
 	}
 	
